d493a7acee3e5341b71de7ace1d5ab04a90ce136
vineethsagar <30525698+vineethsagar@users.noreply.github.com>
2020-12-01 22:28:18 +0530

    update uselocation

--- src/components/LocationDisplay.jsx
+++ src/components/LocationDisplay.jsx
@@ -2,17 +2,17 @@ import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 export default function LocationDisplay() {
-  const [location, setLocation] = useState("");
+  // const [location, setLocation] = useState("");
 
   const loc = useLocation();
-
+  /* 
   useEffect(() => {
     setLocation(loc.pathname);
   }, [setLocation, loc]);
-
+ */
   return (
     <>
-      <h1 data-testid="location-display">{location}</h1>
+      <h1 data-testid="location-display">{loc.pathname}</h1>
     </>
   );
 }
